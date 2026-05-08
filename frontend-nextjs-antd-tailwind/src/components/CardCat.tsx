6ebe1d02cb4d9b29b986434b7891db287a23b5b6
Le Van Dung <dunglv270404@gmail.com>
2026-05-08 08:37:37 +0700

    categories

---
+++ frontend-nextjs-antd-tailwind/src/components/CardCat.tsx
@@ -0,0 +1,59 @@
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { Badge, Card, Tag } from "antd";
+import Link from "next/link";
+
+interface CardCatProps {
+  id: string;
+  icon: string;
+  count: number;
+  isPublic: boolean;
+  color: string;
+  name: string;
+  type?: "category" | "subcategory";
+}
+const CardCat: React.FC<CardCatProps> = ({
+  id,
+  icon,
+  count,
+  isPublic,
+  color,
+  name,
+  type = "category",
+}) => {
+  return (
+    <Link
+      href={`/${type === "category" ? "categories" : "wheels"}/${id}`}
+      className="block"
+    >
+      <Card
+        hoverable
+        className="relative overflow-hidden"
+        style={{ borderTop: `5px solid ${color}` }}
+        actions={[
+          <EditOutlined
+            key="edit"
+            // onClick={(e) => showEditModal(item, e)}
+          />,
+          <DeleteOutlined
+            key="delete"
+            // onClick={(e) => handleDelete(id, e)}
+            className="text-red-500"
+          />,
+        ]}
+      >
+        <div className="flex justify-between items-start">
+          <span className="text-4xl">{icon}</span>
+          <Badge count={count} color="blue" showZero />
+        </div>
+        <div className="mt-4">
+          <h3 className="text-lg font-semibold">{name}</h3>
+          <Tag color={isPublic ? "green" : "default"}>
+            {isPublic ? "Công khai" : "Riêng tư"}
+          </Tag>
+        </div>
+      </Card>
+    </Link>
+  );
+};
+
+export default CardCat;
